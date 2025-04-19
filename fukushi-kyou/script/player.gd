5809bda6634705979c12633aca2c9e2d54e4555f
Supragy352 <mishra03supragya@gmail.com>
2025-04-19 14:54:26 +0530

    asset edits
    Added Assets
    Organised Folders
    Added Firebase Plugin
    Player shaders (blink and hue shift)

--- fukushi-kyou/script/player.gd
+++ fukushi-kyou/script/player.gd
@@ -1,29 +1,40 @@
 extends CharacterBody2D
 
+const SPEED = 300.0
+const JUMP_VELOCITY = -300.0
+var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-@export var speed : float  = 300.0
-@export var jump_velocity : float = -400
-@export var double_jump_velocity : float = -200
+@onready var animated_sprite = $AnimatedSprite2D
 
-var has_double_jump : bool = false
+func SetShader_BlinkIntensity(newValue: float):
+	animated_sprite.material.set_shader_parameter("blink_intensity", newValue)
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity.y += gravity * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("jump"):
-		if is_on_floor():
-			velocity.y = jump_velocity
-			
-			
-
-
-	var direction := Input.get_axis("left", "right")
-	if direction:
-		velocity.x = direction * speed
-	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		var tween = get_tree().create_tween()
+		tween.tween_method(SetShader_BlinkIntensity, 1.0, 0.0, 0.5)
+		velocity.y = JUMP_VELOCITY
+		
 	
+	if velocity.y != 0:
+		animated_sprite.play("jump")
+	else:
+		animated_sprite.play("idle")
+
+
+	var direction := Input.get_axis("ui_left", "ui_right")
+
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, 0.1)
+
 	move_and_slide()
+
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
